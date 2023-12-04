8c928eed983c042d5384ff9dc5823a48642847e2
IvanNikolic21 <ivan.nikolic@sns.it>
2023-12-04 11:20:12 +0100

    Implement upper for loop that controls the number of iterations.

--- main.py
+++ main.py
@@ -480,8 +480,10 @@ if __name__ == '__main__':
             )
         if inputs.use_EW:
             ew_factor, la_e = calculate_EW_factor(Muv.flatten(), beta.flatten(), return_lum=True)
+            print("This is la_e now", la_e)
             ew_factor.reshape((np.shape(Muv)))
             la_e.reshape((np.shape(Muv)))
+            print("and this is it now: ", la_e, "\n with a shape", np.shape(la_e))
             data = np.array(tau_data_I)
         else:
             data = np.array(tau_data_I)
