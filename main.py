7347d436dadc5a35d78479d1325516b132c4d527
IvanNikolic21 <ivan.nikolic@sns.it>
2024-02-21 12:05:06 +0100

    Convergence test <br>
    It seems like convergence is not great for all the things I'm running. Therefore, I've updated my code so that it can calculate a likelihood on the spectrum for a slew of different bin choices. <Br>
    Also some small changes to the tau calculation.

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ def _get_likelihood(
             #     Cosmo.comoving_distance(red_s) - dist * u.Mpc,
             #     ztol=0.00005
             # )
-            z_end_bub = z_at_proper_distance( dist / (1+ redshift) * u.Mpc, red_s)
+            z_end_bub = z_at_proper_distance( dist / (1+ red_s) * u.Mpc, red_s)
         else:
             z_end_bub = red_s
             dist = 0
