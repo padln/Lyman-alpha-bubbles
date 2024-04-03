387abe48baf2ba138f7c2565021352d8d94bbe61
IvanNikolic21 <ivan.nikolic@sns.it>
2024-04-03 14:39:12 +0200

    Debugging consistent noise

--- venv/helpers.py
+++ venv/helpers.py
@@ -479,7 +479,8 @@ def perturb_flux(
         )
     else:
         flux_0 = full_res_resh
+    print("inside recalculating flux", n_bins, flux_0)
     flux_rebin = np.array([np.sum(flux_0[:, wave_em_dig_rebin == j + 1], axis=1) for j in
                   range(n_bins)])
-
+    print("inside recalculating flux, rebined", n_bins, flux_rebin)
     return flux_rebin.reshape((*list(full_res_shape[:-1]), n_bins))
