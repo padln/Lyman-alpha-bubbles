1a7bbecb4cc6f07032e4a1cbbc36bbf3c3d78c2d
IvanNikolic21 <ivan.nikolic@sns.it>
2024-04-22 16:56:59 +0200

    New changes that should in principle affect the speed of the sampler. They deal with pre-computing tau distributions that are connected to the outside bubbles. It's not super obvious it will work, but let's check.

--- venv/speed_up.py
+++ venv/speed_up.py
@@ -1,9 +1,9 @@
 import numpy as np
 from venv.galaxy_prop import get_js, p_EW
 from venv.igm_prop import get_xH, get_bubbles
+from astropy import units as u
 
 wave_em = np.linspace(1214, 1225., 100) * u.Angstrom
-from astropy import units as u
 
 class OutsideContainer:
     def __init__(self):
