caf420a61e2e0375352d7ca46843f9f0f6990411
IvanNikolic21 <ivan.nikolic@sns.it>
2024-07-17 13:15:54 +0200

    Using Lyman-alpha luminosity as a constraint!

--- main.py
+++ main.py
@@ -307,14 +307,16 @@ def _get_likelihood(
                 raise ValueError
 
             if constrained_prior:
-                #if flux_int[index_gal] > flux_limit:
-                for index_tau_for, res_i_for in enumerate(res):
-                    if abs(res_i_for - tau_data[index_gal]) < width_conp:
-                        keep_conp[
-                            index_gal, n * n_inside_tau + index_tau_for] = 1
-                    else:
-                        keep_conp[
-                            index_gal, n * n_inside_tau + index_tau_for] = 0
+                if flux_int[index_gal] > flux_limit:
+                    for index_tau_for, lae_i_for in enumerate(lae_now[
+                        n * n_inside_tau:(n + 1) * n_inside_tau
+                    ]):
+                        if abs((lae_i_for - la_e_in[index_gal])/la_e_in[index_gal]) < width_conp:
+                            keep_conp[
+                                index_gal, n * n_inside_tau + index_tau_for] = 1
+                        else:
+                            keep_conp[
+                                index_gal, n * n_inside_tau + index_tau_for] = 0
 
             #del res
             #del flux_now_i
