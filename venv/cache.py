2efface2d226e341e793c0a1654c89ad6d3fa39b
IvanNikolic21 <ivan.nikolic@sns.it>
2024-07-17 16:30:44 +0200

    Tau now full

--- venv/cache.py
+++ venv/cache.py
@@ -165,7 +165,7 @@ def _get_likelihood_cache(
             output_dir=cache_dir,
         )
         flux_tot.append(np.array(flux_now).flatten())
-        taus_tot.append(np.array(taus_now).flatten())
+        taus_tot.append(np.array(tau_now_full).flatten())
         spectrum_tot.append(spectrum_now)
     try:
         taus_tot_b = []
