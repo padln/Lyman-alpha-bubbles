ef5167eaf2b4a404db0e55ebf03f481f2b78dbb8
IvanNikolic21 <ivan.nikolic@sns.it>
2024-07-21 15:00:52 +0200

    Returning to 5 bins.

--- main.py
+++ main.py
@@ -481,13 +481,13 @@ def _get_likelihood(
                     )
             if like_on_flux is not False:
                 for bin_i in range(2, bins_tot):
-                    if bin_i < 7:
+                    if bin_i < 6:
                         data_to_get = np.log10(
                             1e18 * (5e-19 + spec_line[:, bin_i - 1, 1:bin_i]).T
                         )
                     else:
                         data_to_get = np.log10(
-                            1e18 * (5e-19 + spec_line[:, bin_i - 1, 1:7]).T
+                            1e18 * (5e-19 + spec_line[:, bin_i - 1, 1:6]).T
                         )
                     #print(data_to_get, flush=True)
                     #print("just in case, print", data_to_get[0], flush=True)
@@ -495,7 +495,7 @@ def _get_likelihood(
                     # print(spec_line[:,bin_i-1, 1:bin_i], np.shape(spec_line[:,bin_i-1, 1:bin_i]))
                     spec_kde = gaussian_kde(data_to_get, bw_method=0.2)
 
-                    if bin_i < 7:
+                    if bin_i < 6:
                         data_to_eval = np.log10(
                             (1e18 * (
                                     5e-19 + like_on_flux[ind_data][
@@ -506,8 +506,8 @@ def _get_likelihood(
                         data_to_eval = np.log10(
                             (1e18 * (
                                     5e-19 + like_on_flux[ind_data][
-                                            bin_i - 1, 1:7])
-                            ).reshape(6, 1)
+                                            bin_i - 1, 1:6])
+                            ).reshape(5, 1)
                         )
                     likelihood_spec[:ind_data, bin_i - 1] += np.log(
                         spec_kde.evaluate(
