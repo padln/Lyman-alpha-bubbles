b6234bf36c0871c072e43cfaf385cd8e0356d725
IvanNikolic21 <ivan.nikolic@sns.it>
2024-07-25 12:40:54 +0200

    Fiducial 3 bins

--- venv/cache.py
+++ venv/cache.py
@@ -298,17 +298,17 @@ def _get_likelihood_cache(
         #print(spec_line, flush=True)
         if like_on_flux is not False:
             for bin_i in range(2, bins_tot):
-                if bin_i < 5:
+                if bin_i < 4:
                     data_to_get = np.log10(
                         1e18 * (5e-19 + spec_line[:, bin_i - 1, 1:bin_i]).T
                     )
                 else:
                     data_to_get = np.log10(
-                        1e18 * (5e-19 + spec_line[:, bin_i - 1, 1:5]).T
+                        1e18 * (5e-19 + spec_line[:, bin_i - 1, 1:4]).T
                     )
 
                 spec_kde = gaussian_kde(data_to_get, bw_method=0.2)
-                if bin_i < 5:
+                if bin_i < 4:
                     data_to_eval = np.log10(
                             (1e18 * (
                                 5e-19 + like_on_flux[ind_data][
@@ -319,8 +319,8 @@ def _get_likelihood_cache(
                     data_to_eval = np.log10(
                         (1e18 * (
                                 5e-19 + like_on_flux[ind_data][
-                                        bin_i - 1, 1:5])
-                        ).reshape(4, 1)
+                                        bin_i - 1, 1:4])
+                        ).reshape(3, 1)
                     )
                 likelihood_spec[:ind_data, bin_i - 1] += np.log(
                     spec_kde.evaluate(
