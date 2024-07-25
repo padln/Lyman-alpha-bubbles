0617f91b773a6ceb443fd7b8a615513fb928726e
IvanNikolic21 <ivan.nikolic@sns.it>
2024-07-25 14:58:10 +0200

    more bins

--- venv/cache.py
+++ venv/cache.py
@@ -320,11 +320,11 @@ def _get_likelihood_cache(
             for bin_i in range(2, bins_tot):
                 if bin_i < 7:
                     data_to_get = np.log10(
-                        1e18 * (5e-19 + spec_line[:, bin_i - 1, 1:bin_i]).T
+                        1e18 * (5e-19 + spec_line[:, bin_i - 1, :bin_i]).T
                     )
                 else:
                     data_to_get = np.log10(
-                        1e18 * (5e-19 + spec_line[:, bin_i - 1, 1:7]).T
+                        1e18 * (5e-19 + spec_line[:, bin_i - 1, :7]).T
                     )
 
                 spec_kde = gaussian_kde(data_to_get, bw_method=0.2)
@@ -332,15 +332,15 @@ def _get_likelihood_cache(
                     data_to_eval = np.log10(
                             (1e18 * (
                                 5e-19 + like_on_flux[ind_data][
-                                        bin_i - 1, 1:bin_i])
+                                        bin_i - 1, :bin_i])
                             ).reshape(bin_i - 1, 1)
                         )
                 else:
                     data_to_eval = np.log10(
                         (1e18 * (
                                 5e-19 + like_on_flux[ind_data][
-                                        bin_i - 1, 1:7])
-                        ).reshape(6, 1)
+                                        bin_i - 1, :7])
+                        ).reshape(7, 1)
                     )
                 likelihood_spec[:ind_data, bin_i - 1] += np.log(
                     spec_kde.evaluate(
