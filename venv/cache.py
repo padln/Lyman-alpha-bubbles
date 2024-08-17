b46155fa74af9251d711c2669e842799ac4b6624
IvanNikolic21 <ivan.nikolic@sns.it>
2024-08-17 12:10:46 +0200

    Using higher number

--- venv/cache.py
+++ venv/cache.py
@@ -333,15 +333,15 @@ def _get_likelihood_cache(
             for bin_i in range(2, bins_tot):
                 if bin_i < 6:
                     data_to_get = 5*np.log10(
-                        10**18.7 * (6e-19 + 2*spec_line[:, bin_i - 1, 1:bin_i]).T
+                        10**18.7 * (7e-19 + 2*spec_line[:, bin_i - 1, 1:bin_i]).T
                     )
                 else:
                     data_to_get = 5*np.log10(
-                        10**18.7 * (6e-19 + 2*spec_line[:, bin_i - 1, 1:6]).T
+                        10**18.7 * (7e-19 + 2*spec_line[:, bin_i - 1, 1:6]).T
                     )
-                if np.any(np.isnan(data_to_get)):
+                if np.any(np.isnan(data_to_get.flatten())):
                     print("There was a nan:", data_to_get[np.isnan(data_to_get)])
-                if np.any(np.isinf(data_to_get)):
+                if np.any(np.isinf(data_to_get.flatten())):
                     print("There was infinity:", data_to_get[np.isinf(data_to_get)])
                 spec_kde = gaussian_kde(data_to_get, bw_method=0.25)
                 if bin_i < 6:
