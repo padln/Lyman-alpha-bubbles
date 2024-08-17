c1284ea455806510955326d851107993420bb139
IvanNikolic21 <ivan.nikolic@sns.it>
2024-08-17 12:23:35 +0200

    Print spec line

--- venv/cache.py
+++ venv/cache.py
@@ -340,6 +340,7 @@ def _get_likelihood_cache(
                         10**18.7 * (7e-19 + 2*spec_line[:, bin_i - 1, 1:6]).T
                     )
                 if np.any(np.isnan(data_to_get.flatten())):
+                    print("For this galaxy a nan:",spec_line[:, bin_i - 1, 1:6][np.isnan(data_to_get)] )
                     print("There was a nan:", data_to_get[np.isnan(data_to_get)])
                 if np.any(np.isinf(data_to_get.flatten())):
                     print("There was infinity:", data_to_get[np.isinf(data_to_get)])
